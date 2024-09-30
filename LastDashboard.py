3ef2febbfaba68a1ffcce5c17887530f287741a3
Mirko Calcaterra <mirko-98@libero.it>
2024-09-30 10:04:27 +0200

    Will it work?

--- LastDashboard.py
+++ LastDashboard.py
@@ -14,7 +14,7 @@ import plotly.express as px
 import os
 
 #db_path = "scoutC.db"
-db_filename = "scoutC.db"
+db_filename = "scout/scoutC.db"
 db_path = os.path.abspath(os.path.join(os.getcwd(), db_filename))
 
 print(f"Attempting to connect to database at: {db_path}")
